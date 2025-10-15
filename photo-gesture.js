250a96b98499fc3cf1bc720fc90ca240ff3ab109
Ada <ada@thingvellir.net>
2025-10-15 20:37:59 +1000

    tweak photo gesture threshold

--- photo-gesture.js
+++ photo-gesture.js
@@ -1,4 +1,6 @@
-// To trigger gesture: Double-tap dominant trigger near your ear
+// To trigger gesture:
+// - Double-tap dominant trigger near your ear for a photo
+// - Hold dominant trigger near your ear for a GIF
 "use strict";
 const INPUT_LT_CLICK = Controller.Standard.LTClick;
 const INPUT_RT_CLICK = Controller.Standard.RTClick;
@@ -7,8 +9,14 @@ const INPUT_LEFT_HAND = Controller.Standard.LeftHand;
 const INPUT_RIGHT_HAND = Controller.Standard.RightHand;
 
 const GESTURE_DOUBLE_TAP_TIME_MS = 500;
+const GESTURE_VIDEO_WAIT_MS = 500;
+const PHOTO_ASPECT_RATIO = 16 / 9; // 0 for full framebuffer size
 
+let clicked = false;
+let clicks = 0;
 let lastClickTime = Date.now();
+let makeAnimated = false;
+let animatedWaitTimer;
 
 function triggerGesture(leftHanded) {
 	const INPUT_HAND = leftHanded ? INPUT_LEFT_HAND : INPUT_RIGHT_HAND;
@@ -32,12 +40,20 @@ function triggerGesture(leftHanded) {
 	);
 
 	if (
-		Math.abs(handRel.z) < 0.3 &&
+		Math.abs(handRel.z) < 0.2 &&
 		Math.abs(handRel.y) < 0.3 &&
 		Math.abs(handRel.x) < 0.3
 	) {
-		Controller.triggerHapticPulse(1, 150, 2);
-		Window.takeSnapshot();
+		clicks += 1;
+
+		if (Date.now() > lastClickTime + GESTURE_DOUBLE_TAP_TIME_MS && !makeAnimated) {
+			clicks = 0;
+		}
+
+		if (clicks == 1) {
+			Controller.triggerHapticPulse(1, 150, 2);
+			Window.takeSnapshot(true, makeAnimated, PHOTO_ASPECT_RATIO);
+		}
 	}
 }
 
@@ -45,13 +61,20 @@ Controller.inputEvent.connect((action, value) => {
 	const leftHanded = MyAvatar.getDominantHand() === "left";
 	const INPUT_CLICK = leftHanded ? INPUT_LT_CLICK : INPUT_RT_CLICK;
 
-	if (action === INPUT_CLICK && value > 0.5) {
-		const now = Date.now();
-
-		if (now < lastClickTime + GESTURE_DOUBLE_TAP_TIME_MS) {
+	if (action === INPUT_CLICK) {
+		if (value > 0.5 && !clicked) {
 			triggerGesture(leftHanded);
+			lastClickTime = Date.now();
+			animatedWaitTimer = Script.setTimeout(() => {
+				makeAnimated = true;
+				triggerGesture(leftHanded);
+				makeAnimated = false;
+			}, GESTURE_VIDEO_WAIT_MS);
+		} else if (value < 0.5 && animatedWaitTimer !== undefined) {
+			Script.clearTimeout(animatedWaitTimer);
+			animatedWaitTimer = undefined;
 		}
 
-		lastClickTime = now;
+		clicked = value > 0.5;
 	}
 });
