79dfe6eb123836fba6482d0a0cc8a315a3e14a0e
Ada <ada@thingvellir.net>
2026-03-24 05:50:19 +1000

    world platter polish

--- worldPlatter.js
+++ worldPlatter.js
@@ -1,21 +1,17 @@
 (function() { "use strict"; try {
 	const PLACES_API = "https://mv.overte.org/server";
+	const PROTOCOL = "ViLohxhZMTalcuUKwgs63g=="; // Window.protocolSignature();
+	const FONT = "Roboto";
 	const PLATTER_RADIUS = 2;
+	const ORB_RADIUS = 0.2;
+
 	let self;
 	let visual;
 	let loadingVisual;
-	let worlds = [];
+	let orbs = new Map();
 
-	function createWorldOrb(name, url, userCount) {
-		const randRange = (min, max) => Math.random() * (max - min) + min;
-		const color = userCount > 0 ? [192, 0, 255] : [255, 0, 192];
-
-		const halfRadius = (PLATTER_RADIUS / 2) - 0.3;
-		const pos = [
-			randRange(-halfRadius, halfRadius),
-			randRange(0.3, 1.2),
-			randRange(-halfRadius, halfRadius),
-		];
+	function createWorldOrb(pos, name, url, thumbnailURL, userCount) {
+		const color = userCount > 0 ? [1, 0.25, 1] : [0.25, 0, 1];
 
 		const entity = Entities.addEntity({
 			type: "Sphere",
@@ -23,12 +19,38 @@
 			parentID: self,
 			color: color,
 			alpha: 0.9,
-			dimensions: [0.2, 0.2, 0.2],
+			dimensions: [ORB_RADIUS, ORB_RADIUS, ORB_RADIUS],
 			localPosition: pos,
 			collisionless: true,
 			grab: { grabbable: true },
-			lifetime: 3 * 60,
-			script: `(function(){ this.mousePressOnEntity = () => location.handleLookupString(${JSON.stringify(url)}); })`,
+			script: `(function() {
+	this.mouseDoublePressOnEntity = function(id, ev) {
+		console.info("i was double clicked");
+		if (!ev.isPrimaryButton) { return; }
+		location.handleLookupString(${JSON.stringify(url)});
+	}
+})`,
+		});
+
+		Entities.addEntity({
+			type: "Material",
+			name: `WorldOrb Material: ${name}`,
+			parentID: entity,
+			priority: 1,
+			materialURL: "materialData",
+			materialData: JSON.stringify({
+				materialVersion: 1,
+				materials: {
+					model: "vrm_mtoon",
+					albedo: [0, 0, 0],
+					shade: [0, 0, 0],
+					emissiveMap: thumbnailURL,
+					parametricRim: color,
+					parametricRimFresnelPower: 2,
+					rimLightingMix: 0,
+					cullFaceMode: "CULL_FRONT",
+				},
+			}),
 		});
 
 		Entities.addEntity({
@@ -49,10 +71,55 @@
 			collisionless: true,
 			textEffect: "outline fill",
 			textEffectColor: [0, 0, 0],
-			textEffectThickness: 0.3,
+			textEffectThickness: 0.45,
+			font: FONT,
 		});
 
-		worlds.push(entity);
+		orbs.set(entity, { url });
+	}
+
+	function spawnOrbs(places) {
+		const N = places.length;
+		const R = (PLATTER_RADIUS / 2);
+		const phi = (1 + Math.sqrt(5)) / 2;
+		let points = [];
+
+		// sprinkle the orbs with some fibonacci
+		for (let i = 0; i < N; i++) {
+			const j = i / N;
+			const theta = Math.acos(1 - (2 * j));
+			const azimuth = 2 * Math.PI * (i / phi);
+
+			// my brain is too smooth for this shit
+			const r = Math.cbrt(Math.random() * R);
+
+			points.push([
+				r * Math.sin(theta) * Math.cos(azimuth),
+				(r * Math.sin(theta) * Math.sin(azimuth)) + R + (ORB_RADIUS / 2),
+				r * Math.cos(theta),
+			]);
+		}
+
+		for (let i = 0; i < N; i++) {
+			const pos = points[i];
+			const place = places[i];
+
+			createWorldOrb(
+				pos,
+				place.name,
+				`hifi://${place.name}${place.path}`,
+				place.thumbnail,
+				place.current_attendance
+			);
+
+			// debug
+			/*Entities.addEntity({
+				type: "Sphere",
+				parentID: self,
+				localPosition: pos,
+				dimensions: [0.05, 0.05, 0.05],
+			});*/
+		}
 	}
 
 	this.preload = function(_self) {
@@ -69,6 +136,26 @@
 			grab: { grabbable: false },
 			ignorePickIntersection: true,
 			collisionless: true,
+		});
+
+		Entities.addEntity({
+			type: "Material",
+			parentID: visual,
+			name: "WorldPlatter Visual Material",
+			priority: 1,
+			materialURL: "materialData",
+			materialData: JSON.stringify({
+				materialVersion: 1,
+				materials: {
+					model: "vrm_mtoon",
+					albedo: [0.54, 0.32, 0],
+					shade: [0, 0, 0],
+					opacity: 0.9,
+					parametricRim: [1, 0.54, 0],
+					parametricRimFresnelPower: 2,
+					rimLightingMix: 0,
+				},
+			}),
 		});
 
 		loadingVisual = Entities.addEntity({
@@ -89,23 +176,25 @@
 			grab: { grabbable: false },
 			ignorePickIntersection: true,
 			collisionless: true,
+			font: FONT,
 		});
+
+		// debug
+		/*spawnOrbs([]);
+		Entities.deleteEntity(loadingVisual);
+		loadingVisual = undefined;*/
 
 		let xhr = new XMLHttpRequest();
 
 		xhr.timeout = 10 * 1000;
 		xhr.onreadystatechange = () => {
-			if (xhr.readyState !== 4 /* DONE */) { return; }
+			if (xhr.readyState !== 4) { return; }
 
 			try {
 				console.debug(`Parsing places list: ${xhr.status} ${xhr.statusText} ${xhr.errorCode}`);
-				const protocol = "ViLohxhZMTalcuUKwgs63g=="; // Window.protocolSignature();
 				const places = JSON.parse(xhr.responseText).data.places;
 
-				for (const place of places) {
-					if (place.domain.protocol_version !== protocol) { continue; }
-					createWorldOrb(place.name, `hifi://${place.name}${place.path}`, place.current_attendance);
-				}
+				spawnOrbs(places.filter(a => a.domain.protocol_version === PROTOCOL));
 
 				Entities.deleteEntity(loadingVisual);
 				loadingVisual = undefined;
@@ -125,8 +214,8 @@
 		Entities.deleteEntity(visual);
 		Entities.deleteEntity(loadingVisual);
 
-		for (const world of worlds) {
-			Entities.deleteEntity(world);
+		for (const orb of orbs.keys()) {
+			Entities.deleteEntity(orb);
 		}
 	};
 
